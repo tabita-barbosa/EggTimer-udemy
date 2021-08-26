07594061ebdbed5d6d1246af03a16bc32b9ac131
Tabita <tabita.barbosa8@gmail.com>
2021-08-26 02:06:10 -0300

    finaliza projeto

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,25 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    
+    // 300, 420, 720
+    let eggTime : [String : Int] = ["Soft": 4, "Medium": 5, "Hard": 6]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    var player: AVAudioPlayer!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        
-        let hardness = sender.currentTitle
-        
-        if hardness == "Soft" {
-            print(softTime)
-        } else if hardness == "Medium" {
-            print(mediumTime)
-        } else {
-            print(hardTime)
-        }
-        
-    }
 
+        timer.invalidate()
+        let hardness = sender.currentTitle
+        if hardness != nil{
+            totalTime = eggTime[hardness!]!
+        }
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        // time interval = intervalo que queremos atualizar, queremos repetir a cada segundo, o selector é do objectiveC, aceita um nome de funcão como parametro
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    // cada vez que ativa a adiciona um segundo
+    @objc func updateTimer(){
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+            alarm(soundName: "alarm_sound")
+        }
+    }
+    
+    func alarm (soundName: String){
+        let url = Bundle.main.url(forResource: soundName, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
 }
